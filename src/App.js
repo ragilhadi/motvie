2e60d27654d0d745b7046d4f27db979b774c5ae3
ragilhadi <ragilprasetyo310@gmail.com>
2020-07-17 17:32:28 +0700

    Create Card Movie and TV

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Footer from "./components/footer/Footer";
 import CardPerson from "./components/card-person/CardPerson";
 import BannerDetails from "./components/banner-details/BannerDetails";
+import CardMovieTV from "./components/card-movie-tv/CardMovieTV";
 
 function App() {
   return (
     <div className="App">
       <BannerDetails />
       <CardPerson />
+      <CardMovieTV />
       <Footer />
     </div>
   );
---
+++ src/components/card-movie-tv/CardMovieTV.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./CardMovieTV.scss";
+
+const CardMovieTV = () => {
+  return (
+    <div className="card-movieTV">
+      <div className="rating">
+        <p>7.5</p>
+      </div>
+    </div>
+  );
+};
+
+export default CardMovieTV;
