e3f8a6f4fc3315f2d5ab3449ac58fe7891606579
ragilhadi <ragilprasetyo310@gmail.com>
2020-07-20 01:37:28 +0700

    Create HighlightHome

--- src/components/highlight-home/HighlightHome.js
+++ src/components/highlight-home/HighlightHome.js
@@ -1,20 +1,28 @@
 import React from "react";
 import ButtonPrimary from "../button-primary/ButtonPrimary";
+import Backdrop from "../../assets/backdrop.jpg";
 import "./HighlightHome.scss";
 import ButtonSecondary from "../button-secondary/ButtonSecondary";
 
 const HighlightHome = () => {
   return (
     <div className="highlight">
-      <h1>Train to Busan</h1>
-      <p>
-        A first-time captain leads a convoy of allied ships carrying thousands
-        of soldiers across the treacherous waters of the “Black Pit
-      </p>
-      <div>
-        <ButtonPrimary>Details</ButtonPrimary>
-        <ButtonSecondary>Play Trailer</ButtonSecondary>
+      <div className="info">
+        <div className="description">
+          <h1>Train to Busan</h1>
+          <p>
+            A first-time captain leads a convoy of allied ships carrying
+            thousands of soldiers across the treacherous waters of the “Black
+            Pit
+          </p>
+        </div>
+        <div className="buttons">
+          <ButtonPrimary className="button-1">Details</ButtonPrimary>
+          <ButtonSecondary>Play Trailer</ButtonSecondary>
+        </div>
       </div>
+      <div className="overlay"></div>
+      <img src={Backdrop} alt="backdrop" className="backdrop" />
     </div>
   );
 };
