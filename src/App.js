826a7f809a806f55fe51df90b049619083316c9c
ragilhadi <ragilprasetyo310@gmail.com>
2020-07-21 05:23:50 +0700

    create navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from "react";
 import Footer from "./components/footer/Footer";
-import CardPerson from "./components/card-person/CardPerson";
-import BannerDetails from "./components/banner-details/BannerDetails";
-import CardMovieTV from "./components/card-movie-tv/CardMovieTV";
-import HighlightHome from "./components/highlight-home/HighlightHome";
 import Row from "./components/row/Row";
 import request from "./api/request";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <HighlightHome />
+    <div>
+      <Navbar />
       <Row type="Netlix" genre="Originals" url={request.fetchNetflixOri} />
       <Row type="Discover" genre="Trending" url={request.fetchTrending} />
       <Footer />
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Navbar.scss";
+import { MdMovie, MdPerson, MdCollections, MdSlideshow } from "react-icons/md";
 
 const Navbar = () => {
-  return <div></div>;
+  const [shadow, setshadow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 50) {
+        setshadow(true);
+      } else setshadow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  return (
+    <>
+      <div className={`navbar-container ${shadow && "nav-shadow"}`}>
+        <div className="section-1">
+          <div className="logo">
+            <h1>Motvie</h1>
+          </div>
+          <div className="options">
+            <a href="#">Movies</a>
+            <a href="#">Tv Show</a>
+            <a href="#">Person</a>
+            <a href="#">Collection</a>
+          </div>
+        </div>
+        <div className="section-2">
+          <input />
+        </div>
+      </div>
+
+      <div className="bottom-navigation">
+        <div className="movie">
+          <MdMovie />
+          <p>Movies</p>
+        </div>
+        <div className="TVShow">
+          <MdSlideshow />
+          <p>TV Show</p>
+        </div>
+        <div className="Person">
+          <MdPerson />
+          <p>Person</p>
+        </div>
+        <div className="Collection">
+          <MdCollections />
+          <p>Collection</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
