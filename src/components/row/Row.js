60683f4630425760cdb836799d7def2650ec6173
Ragil Hadi Prasetyo <ragilprasetyo310@gmail.com>
2020-08-13 17:12:55 +0700

    Setup for gh pages

--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from "react";
 import "./Row.scss";
 import CardMovieTV from "../card-movie-tv/CardMovieTV";
+import { BeatLoader } from "react-spinners";
 import axios from "../../api/axios";
 
 const Row = ({ type, genre, url }) => {
   const [movies, setmovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
-      const request = await axios.get(url);
-      setmovies(request.data.results);
+      const request = await axios
+        .get(url)
+        .then((result) => setmovies(result.data.results));
+      setIsLoading(false);
+      // setmovies(request.data.results);
       return request;
     }
     fetchData();
@@ -18,7 +23,20 @@ const Row = ({ type, genre, url }) => {
   return (
     <div className="row-container">
       <div className="scroll-container">
-        {movies.map((data) => (
+        {isLoading ? (
+          <BeatLoader />
+        ) : (
+          movies.map((data) => (
+            <CardMovieTV
+              key={data.id}
+              image={data.poster_path}
+              name={data.original_title}
+              id={data.id}
+              vote={data.vote_average}
+            />
+          ))
+        )}
+        {/* {movies.map((data) => (
           <CardMovieTV
             key={data.id}
             image={data.poster_path}
@@ -26,7 +44,7 @@ const Row = ({ type, genre, url }) => {
             id={data.id}
             vote={data.vote_average}
           />
-        ))}
+        ))} */}
       </div>
     </div>
   );
