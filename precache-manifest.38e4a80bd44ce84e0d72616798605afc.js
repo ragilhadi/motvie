e8afe9499becdf1502bcfcce04a4cea0c104c9f9
Ragil Hadi Prasetyo <ragilprasetyo310@gmail.com>
2020-08-14 16:39:52 +0700

    Updates

---
+++ precache-manifest.38e4a80bd44ce84e0d72616798605afc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e394a5922950708c7d17a8f734dafba",
+    "url": "/motvie/index.html"
+  },
+  {
+    "revision": "6c332e5d1b8e262a95e9",
+    "url": "/motvie/static/css/main.99132a9b.chunk.css"
+  },
+  {
+    "revision": "91fd743484f3b2a1487b",
+    "url": "/motvie/static/js/2.ec52bba5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/motvie/static/js/2.ec52bba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c332e5d1b8e262a95e9",
+    "url": "/motvie/static/js/main.fc28b049.chunk.js"
+  },
+  {
+    "revision": "0659fab6da01663b1e43",
+    "url": "/motvie/static/js/runtime-main.6ac4eca1.js"
+  },
+  {
+    "revision": "37c3a95b9dd80b15533f6e24ccf85acd",
+    "url": "/motvie/static/media/home.37c3a95b.png"
+  },
+  {
+    "revision": "468ddc629ee89c181edb6abfc891bfc6",
+    "url": "/motvie/static/media/moviedb.468ddc62.jpg"
+  }
+]);
