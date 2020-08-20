746010d579dbb616725027f185b981e6437215be
Ragil Hadi Prasetyo <ragilprasetyo310@gmail.com>
2020-08-20 13:54:54 +0700

    Fix some problem with 404.html

--- src/components/card-landscape/CardLanscape.js
+++ src/components/card-landscape/CardLanscape.js
@@ -1,19 +1,29 @@
 import React from "react";
 import "./CardLanscape.scss";
+import { Link } from "react-router-dom";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-const CardLanscape = ({ photo, name, overview }) => {
+const CardLanscape = ({ photo, name, overview, id }) => {
   function trimString(string, n) {
     return string.substring(0, n) + "...";
   }
   return (
     <div className="card-lanscape">
-      <img src={`${base_url}${photo}`} className="image" alt="backdrop" />
-      <div className="details-card">
-        <p className="title">{name}</p>
-        <p className="description">{trimString(String(overview), 80)}</p>
-      </div>
+      <Link
+        to={{
+          pathname: `/detail/${id}`,
+          state: {
+            identification: id,
+          },
+        }}
+      >
+        <img src={`${base_url}${photo}`} className="image" alt="backdrop" />
+        <div className="details-card">
+          <p className="title">{name}</p>
+          <p className="description">{trimString(String(overview), 80)}</p>
+        </div>
+      </Link>
     </div>
   );
 };
